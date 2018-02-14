cc7c3ce25bd4663aedf1359a7219fdf43168555f
Sergey Bogolepov <ace.is.not.my.card@gmail.com>
2018-02-14 14:24:13 +0300

    Boxing of bytes, shorts, chars, ints and longs

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/IrToBitcode.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/IrToBitcode.kt
@@ -339,6 +339,7 @@ internal class CodeGeneratorVisitor(val context: Context, val lifetimes: Map<IrE
     override fun visitModuleFragment(declaration: IrModuleFragment) {
         context.log{"visitModule                    : ${ir2string(declaration)}"}
 
+        BoxCache.initialize(context)
         declaration.acceptChildrenVoid(this)
 
         // Note: it is here because it also generates some bitcode.
@@ -427,7 +428,6 @@ internal class CodeGeneratorVisitor(val context: Context, val lifetimes: Map<IrE
         // TODO: collect those two in one place.
         context.llvm.fileInitializers.clear()
         context.llvm.objects.clear()
-
         using(FileScope(declaration)) {
             declaration.acceptChildrenVoid(this)
 
@@ -2030,6 +2030,10 @@ internal class CodeGeneratorVisitor(val context: Context, val lifetimes: Map<IrE
             }
             "konan.internal.getContinuation" -> return getContinuation()
         }
+
+        BoxCache.getCacheByInRangeChecker(name)?.let { cache -> return cache.inRange(functionGenerationContext, args[0]) }
+
+        BoxCache.getCacheByBoxGetter(name)?.let { cache -> return cache.getCachedValue(functionGenerationContext, args[0]) }
 
         val interop = context.interopBuiltIns
 
